76d7fa6b55e52e2118b035e810c2796ae086d101
Kyle Cox <cox.kyle@gmail.com>
2021-10-15 09:48:27 -0400

    Submission for Lab 3

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Lab03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isBadNumber;
+            bool playAgain = true;
+            int number = -1;
+
+            Console.WriteLine("Hi! What's your name?");
+            string userName = Console.ReadLine();
+
+            do // loop for play again
+            {
+                isBadNumber = true;
+                while (isBadNumber == true)
+                {
+                    Console.WriteLine($"{userName}, please enter a whole number between 1 and 100.");
+                    string userInput = Console.ReadLine();
+
+                    if (!int.TryParse(userInput, out number))
+                    {
+                        Console.WriteLine($"Sorry, no decimals or non-number characters please.");
+                    }
+                    else if (number < 1 || number > 100)
+                    {
+                        Console.WriteLine($"Sorry, that number was out of range.");
+                    }
+                    else
+                    {
+                        isBadNumber = false;
+                    }
+                }
+
+                if (number % 2 != 0)
+                {
+                    Console.WriteLine($"{number} and Odd.");
+                }
+                else if (number % 2 == 0 && number >= 2 && number <= 25)
+                {
+                    Console.WriteLine($"Even and less than 25.");
+                }
+                else if (number % 2 == 0 && number >= 26 && number <= 60)
+                {
+                    Console.WriteLine($"Even.");
+                }
+                else if (number % 2 == 0 && number >= 60)
+                {
+                    Console.WriteLine($"{number} and Even.");
+                }
+                else if (number % 2 != 0 && number >= 60)
+                {
+                    Console.WriteLine($"{number} and Odd.");
+                }
+
+                bool isValidYesNo = false;
+                do // loop while getting a valid playAgain input
+                {
+                    Console.WriteLine("Continue? (y/n)");
+                    string input = Console.ReadLine();
+                    if (input.Trim().ToLower() == "n")
+                    {
+                        playAgain = false;
+                        isValidYesNo = true;
+                    }
+                    else if (input.Trim().ToLower() == "y")
+                    {
+                        isValidYesNo = true;
+                    } else
+                    {
+                        Console.WriteLine($"Please just enter a 'y' or an 'n', {userName}.");
+                    }
+                } while (isValidYesNo == false);
+
+
+            } while (playAgain == true);
+
+            Console.WriteLine($"Thanks for playing, {userName}! Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
